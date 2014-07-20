942e841082809e916938bf5222cc0be62b2603ce
Eric <ericshi2009@gmail.com>
2014-07-20 02:33:01 -0700

    Making debug toolbar live

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
 
 DISQUS_API_KEY = 'Yyf6oulJOoLrIz3T4guhsL6up2DBzRopIXgsH2piNpHphZWrGnFivK4n1YTxW49R'
 DISQUS_WEBSITE_SHORTNAME = 'ericshiblog'
-INTERNAL_IPS = "http://127.0.0.1:8000/"
+INTERNAL_IPS = "http://127.0.0.1:8000/","www.ericshi.ca"
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
